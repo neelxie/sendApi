5d6085d3ad9a99fb4901316e16c07ec0341009a6
Sekidde Derrick <kidricederek@gmail.com>
2018-11-09 10:27:19 +0300

    [finishes #161780461] [finishes #161780472] [finishes #161780425] get all orders. Get specific order. Create order.

---
+++ sendit/models/order_model.py
@@ -0,0 +1,7 @@
+""" Model class for orders that returns a list that holds all orders."""
+
+class Orders:
+    """ Model class for orders. """
+    def __init__(self):
+        """ Initialisation method for Orders class."""
+        self.orders_list = []
--- sendit/routes.py
+++ sendit/routes.py
@@ -6,6 +6,8 @@ from flask_restful import Resource
 from sendit.views.index_view import Index
 from sendit.views.user_view import OneUser
 from sendit.views.user_view import AllUsers
+from sendit.views.order_view import OneOrder
+from sendit.views.order_view import AllOrders
 
 
 api_bp = Blueprint('api', __name__)
@@ -14,3 +16,5 @@ api = Api(api_bp)
 api.add_resource(Index, '/')
 api.add_resource(AllUsers, '/users')
 api.add_resource(OneUser, '/users/<int:user_id>')
+api.add_resource(AllOrders, '/orders')
+api.add_resource(OneOrder, '/orders/<int:order_id>')
