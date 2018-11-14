45c03ae4833432f416630ca4e05b96565226eddb
Sekidde Derrick <kidricederek@gmail.com>
2018-11-14 14:48:22 +0300

    Cancel Endpoint [finishes #161875228]

--- sendit/routes/parcel_route.py
+++ sendit/routes/parcel_route.py
@@ -11,10 +11,15 @@ def get_parcels():
 
 @app.route('/api/v1/parcels/<int:parcel_id>', methods=["GET"])
 def get_single_parcel(parcel_id):
-    """ Method route to Parcel by ID """
+    """ Method route to get Parcel by ID """
     return obj_parcel_views.fetch_parcel_by_id(parcel_id)
 
 @app.route('/api/v1/parcels', methods=["POST"])
 def create_parcel():
-    """ Method route to Parcel by ID """
+    """ Method route to create a Parcel."""
     return obj_parcel_views.add_parcels()
+
+@app.route('/api/v1/parcels/<int:parcel_id>/cancel', methods=["PUT"])
+def cancel_parcel(parcel_id):
+    """ Method route to cancel a Parcel."""
+    return obj_parcel_views.cancel_specific_parcel(parcel_id)
