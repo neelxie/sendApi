2b51e4b5c29e6da98b7a75b36f2a0a27c7d22d1c
Sekidde Derrick <kidricederek@gmail.com>
2018-11-14 09:36:08 +0300

    Get single parcel by ID [finishes #161875175]

--- tests/test_get_parcel.py
+++ tests/test_get_parcel.py
@@ -1,15 +1,15 @@
-""" File to handles tests for get all parcels endpoint """
+""" File to handles tests for get single parcel endpoint """
 import unittest
 from sendit import app
 
-class TestAllParcels(unittest.TestCase):
-    """ Test Class for get all Parcels endpoint"""
+class TestSingleParcel(unittest.TestCase):
+    """ Test Class for single parcel endpoint"""
 
     def setUp(self):
         """ set up method for test cases."""
         self.app = app.test_client()
 
-    def test_get_all_parcels(self):
+    def test_get_single_parcel(self):
         """ Test method to test index endpoint of the app."""
         response = self.app.get('/api/v1/parcels')
         self.assertEqual(response.status_code, 200)
